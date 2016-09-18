42ec1ff8c9a48451ee8c5591e035793079de1570
Petr Gladkiy <petr.gladkiy@gmail.com>
2016-09-18 16:03:26 +0700

    Add windows script to 'build' md files.

---
+++ build.cmd
@@ -0,0 +1,33 @@
+@ECHO OFF
+REM Converts all *.md files to .output/*.html.
+REM Markdown package is installed into .venv virtual environment.
+REM Requires Python3.
+
+SETLOCAL
+
+SET _venv=.venv
+
+IF NOT EXIST %_venv% (
+    py -3 -m venv --system-site-packages %_venv%
+    CALL :PYTHONCALL pip install Markdown
+)
+
+SET _output=.output
+IF NOT EXIST %_output% (
+    MKDIR %_output%
+)
+
+FOR /F "tokens=*" %%G IN ('dir /b *.md') DO (
+    CALL :PYTHONCALL markdown -f %_output%\%%G.html %%G
+)
+
+ENDLOCAL
+
+exit /b 0
+
+REM Calls python 3 module from virtual environment.
+:PYTHONCALL
+SETLOCAL
+%_venv%\Scripts\python.exe -m %*
+ENDLOCAL
+GOTO :EOF
